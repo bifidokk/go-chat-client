1e5f165fc86ac101fdc6030c54a901bdb8762f54
Danil <bifidokk@gmail.com>
2019-12-12 17:58:00 +0500

    Add websocket service and nebular styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {WebsocketService} from './websocket';
+import {WS} from './websocket.events';
+
+export interface IMessage {
+  id: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-chat-app';
+
+export class AppComponent implements OnInit {
+  private messages$: Observable<IMessage[]>;
+
+  constructor(
+      private wsService: WebsocketService
+  ) {
+  }
+
+  ngOnInit() {
+    // get messages
+    this.messages$ = this.wsService.on<IMessage[]>(WS.ON.MESSAGES)
+    this.messages$.subscribe(console.log);
+  }
+
+  public sendMessage(): void {
+    this.wsService.send(WS.SEND.SEND_TEXT, 'My Message Text');
+  }
 }
